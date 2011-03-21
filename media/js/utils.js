e38ecbc73ef2b8ab5bb4ef1648ae2239285a747b
ecoutu <ecoutu@51c1fbb8-2106-49f1-bbdb-2ff3067f671d>
2011-03-21 07:29:19 +0000

    Added a class: mceNoEditor, to prevent TinyMCE from formatting textarea's

--- media/js/utils.js
+++ media/js/utils.js
@@ -17,6 +17,7 @@ tinyMCE.init({
         theme_advanced_resize_horizontal: false,
         theme_advanced_resizing_use_cookie: true,
         theme_advanced_resizing: true,
+        editor_deselector "mceNoEditor",
 });
 
 
