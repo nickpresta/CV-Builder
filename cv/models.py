636f3a57b03d2a19aa6f60940ee577bd31735dc7
ecoutu <ecoutu@51c1fbb8-2106-49f1-bbdb-2ff3067f671d>
2011-03-23 03:22:22 +0000

    Fixed up new models.py and integrated it into project

--- cv/models.py
+++ cv/models.py
@@ -44,4 +44,87 @@ class Summary(models.Model):
     executive = models.TextField()
 
 
+class FacultyTable(models.Model):
+    #Faculty_ID = models.IntegerField()
+    Faculty_GName = models.CharField( max_length=200 )
+    Faculty_SName = models.CharField( max_length=200 )
+    Review_Term = models.IntegerField()
+    Department = models.CharField( max_length=200 ) #TODO: Pre-pop?
+    Faculty_Start = models.DateTimeField(  ) #TODO: trigger
+    
+class DoETable(models.Model):
+    #DoE_ID = models.IntegerField() #TODO: 
+    Faculty_ID = models.ForeignKey( FacultyTable )
+    Year = models.IntegerField( max_length=4 )
+    Research = models.IntegerField()
+    Teaching = models.IntegerField()
+    Service = models.IntegerField()
+    
+class SummaryTable(models.Model):
+    Faculty_ID = models.ForeignKey( FacultyTable )
+    Executive = models.CharField( max_length=10000 )
+    Research  = models.CharField( max_length=10000 )
+    R_Consulting = models.CharField( max_length=10000 )
+    R_Patents = models.CharField( max_length=10000 )
+    R_Other = models.CharField( max_length=10000 )
+    R_Recognition = models.CharField( max_length=10000 )
+    Teaching = models.CharField( max_length=10000 )
+    T_Counselling = models.CharField( max_length=10000 )
+    T_CourseDevel = models.CharField( max_length=10000 )
+    T_Recognition = models.CharField( max_length=10000 )
+    T_Support = models.CharField( max_length=10000 )
+    T_Scholarship = models.CharField( max_length=10000 )
+    T_Offer = models.CharField( max_length=10000 )
+    OffCampus = models.CharField( max_length=10000 )
 
+class AccredTable(models.Model):
+    #Accred_ID = models.IntegerField()
+    Faculty_ID = models.ForeignKey( FacultyTable )
+    Degree = models.CharField( max_length=200 )
+    Discipline = models.CharField( max_length=200 )
+    Institution = models.CharField( max_length=200 )
+    Date = models.DateTimeField() #TODO: Trigger
+    
+class HonorTable(models.Model):
+    #Honor_ID = models.IntegerField()
+    Faculty_ID = models.ForeignKey( FacultyTable )
+    Honor_desc = models.CharField( max_length=500 )
+    
+class PositionTable(models.Model):
+    #Pos_ID = models.IntegerField()
+    Faculty_ID = models.ForeignKey( FacultyTable )
+    Rank = models.CharField( max_length=200 )
+    StartDate = models.DateTimeField() #TODO: Trigger
+    EndDate = models.DateTimeField()
+    Location = models.CharField( max_length=200 )
+    
+class GrantTable(models.Model):
+    #Grant_ID = models.IntegerField()
+    PInvest = models.IntegerField()
+    SYear = models.DateTimeField() #TODO: Trigger
+    EYear = models.DateTimeField() #TODO: Trigger
+#TODO:  Amount = Formatted Float
+
+class InvestTable(models.Model):
+    #Invest_ID = models.IntegerField()
+    Invest_Name = models.CharField( max_length=200 )
+    Grant_ID = models.IntegerField()
+#TODO:  Portion Formatted Float
+ 
+class CourseTable(models.Model): 
+    #CID = models.IntegerField()
+    CCode = models.CharField( max_length=200 )
+    Semester = models.CharField( max_length=200 )
+    Year = models.DateTimeField() #TODO: Trigger
+    Name = models.CharField( max_length=200 )
+    Info = models.CharField( max_length=200 )
+    NumStudents = models.IntegerField()
+ 
+class GradTable:
+    #GID = models.IntegerField()
+    Faculty_ID = models.IntegerField()
+    GName = models.CharField( max_length=200 )
+    GDegree = models.CharField( max_length=200 )
+    SDate = models.DateTimeField() #TODO: Trigger
+    EDate = models.DateTimeField() #TODO: Trigger
+    Note = models.CharField( max_length=200 )
