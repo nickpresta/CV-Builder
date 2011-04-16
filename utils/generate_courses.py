1b971ff7144ce5388b18cf63a948ed537be7e2b2
ecoutu <ecoutu@51c1fbb8-2106-49f1-bbdb-2ff3067f671d>
2011-04-16 09:59:27 +0000

    Modified generate_courses to use JSON instead of YAML.

--- utils/generate_courses.py
+++ utils/generate_courses.py
@@ -2,12 +2,20 @@ import json
 import re
 import urllib2
 
-from BeautifulSoup import BeautifulSoup
+try:
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    print """
+Please install BeautifulSoup module:
+easy_install beautifulsoup"""
+    import sys
+    sys.exit()
 
 url = "http://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12"
 
-soup = BeautifulSoup(urllib2.urlopen(url).read())
+data = []
 
+soup = BeautifulSoup(urllib2.urlopen(url).read())
 
 i = 0
 
@@ -20,22 +28,24 @@ for li in soup.find('div', 'subnav').findAll('li'):
         code = title.groups(0)[0] + title.groups(0)[1]
         title = title.groups(0)[2].rpartition(' ')[0].replace(':', '').replace('&', '')
         
-        print '- model: cv.course'        
-        print '  pk: %d' % i
-        print '  fields:'
-        print '    code: %s' % code
-        print '    name: %s' % title
-        print '    info: '
-        i += 1
+        data.append({
+            'model': 'cv.course',
+            'pk': '%d' % i,
+            'fields': {
+                'code': code,
+                'name': title,
+                'info': ''
+            }
+        })
+        
+#       Replace data.append with following for yaml        
+#        print '- model: cv.course'        
+#        print '  pk: %d' % i
+#        print '  fields:'
+#        print '    code: %s' % code
+#        print '    name: %s' % title
+#        print '    info: '
+#        i += 1
 
-
-#(?![FWS](,[FWS]))
-
-# Outputs the data in a YAML fixture that can be used to import into the
-# database
-
-#for i, dept in enumerate(lis[9:]):
-#    print '- model: cv.departmentchoice'
-#    print '  pk: %d' % (int(i) + 1)
-#    print '  fields:'
-#    print '    name: %s' % dept
+        
+print json.dumps(data)
